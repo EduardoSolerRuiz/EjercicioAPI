97213e9fdf63b6329485d3e3553770d5bad84dc5
Eduardo Soler Ruiz <6669217@alu.murciaeduca.es>
2024-06-06 12:28:00 +0200

    mostrar home por defecto, y añadir a la seccion home algunos favoritos

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const sections = document.querySelectorAll("#main>section")
 // console.log(sections)
 
 
-// const homeSection = document.getElementById('homeSection')
+const homeSection = document.getElementById('homeSection')
 // const searchSection = document.getElementById('searchSection')
 // const favoritesSection = document.getElementById('favoritesSection')
 
@@ -33,6 +33,8 @@ const actualizarTiempo = document.querySelector('#updateFavoritesButton')
 let favoritos = []
 let jsonResultados
 
+let home = []
+
 // function hideAllSections() {
 //     homeSection.style.display = 'none'
 //     searchSection.style.display = 'none'
@@ -43,10 +45,15 @@ function ocultarTodasSecciones() {
     sections.forEach(section => section.style.display = 'none');
 }
 
+// mostrar seccion home por defecto
+
+ocultarTodasSecciones()
+sections[0].style.display = "block"
 
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function() {
+
         ocultarTodasSecciones()
 
         sections[i].style.display = "block"
@@ -131,18 +138,28 @@ mostsrarResultados.addEventListener("click", function(ev) {
 
 })
 
-function añadirSeccionFavoritos(id){
-    // FavoriosListar.innerHTML = ""
-    let ciudad = jsonResultados.list.find(r => r.id == id)
-    if (ciudad) {
-        let crearDiv = document.createElement('div')
-        let botonBorrarFav = document.createElement('button')
+    function añadirSeccionFavoritos(id) {
+        // FavoriosListar.innerHTML = ""
+        let ciudad = jsonResultados.list.find(r => r.id == id)
+        if (ciudad) {
+            const crearDiv = document.createElement('div')
+            const botonBorrarFav = document.createElement('button')
             botonBorrarFav.textContent = "X"
-        crearDiv.textContent = `${ciudad.name}, ${ciudad.sys.country}: ${ciudad.main.temp_min}°C , ${ciudad.main.temp_max}°C`
-        FavoriosListar.append(crearDiv)
-        crearDiv.append(botonBorrarFav)
+
+            // crear boton para añadir a la seccion home
+
+            const botonHome = document.createElement('button')
+            botonHome.textContent = "Añadir a Home"
+            botonHome.dataset.id = ciudad.id
+            botonHome.classList.add('addHome')
+
+
+            crearDiv.textContent = `${ciudad.name}, ${ciudad.sys.country}: ${ciudad.main.temp_min}°C , ${ciudad.main.temp_max}°C`
+            FavoriosListar.append(crearDiv)
+            crearDiv.append(botonBorrarFav)
+            crearDiv.append(botonHome)
+        }
     }
-}
 
 actualizarTiempo.addEventListener('click', function(){
     actualizarTiempoFavoritos()
@@ -160,8 +177,6 @@ function actualizarTiempoFavoritos() {
             botonBorrarFav.textContent = "X"
             botonBorrarFav.dataset.id = favId
             botonBorrarFav.classList.add('borrarFav')
-            
-            
             crearDiv.textContent = `${data.name}, ${data.sys.country}: ${data.main.temp_min}°C , ${data.main.temp_max}°C`
             crearDiv.append(botonBorrarFav)
 
@@ -180,6 +195,30 @@ function actualizarTiempoFavoritos() {
         })
     })
 }
+
+    // funcion para añadir a la seccion home
+    function addHome(id) {
+        let ciudad = jsonResultados.list.find(r => r.id == id)
+        if (ciudad){
+            const crearDivHome = document.createElement('div')
+            crearDivHome.textContent = `${ciudad.name}, ${ciudad.sys.country}: ${ciudad.main.temp_min}°C , ${ciudad.main.temp_max}°C`
+            homeSection.append(crearDivHome)
+
+        }
+    }
+
+    //evento para añadir a la seccion home
+    FavoriosListar.addEventListener("click", function(ev) {
+        if(ev.target.nodeName.toLowerCase() == "button"){
+            let ciudadId = ev.target.dataset.id
+            if (!home.includes(ciudadId)) {
+                home.push(ciudadId)
+                addHome(ciudadId)
+            }
+        }
+    })
+
+    
 
 
 })
